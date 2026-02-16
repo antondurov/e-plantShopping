00d4dd70e8bab4776769c7255ac43c0e77364b1b
Anton <antondurov05@protonnmail.com>
2026-02-16 12:36:10 -0500

    added display for plants

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -274,8 +274,21 @@ function ProductList({ onHomeClick }) {
             </div>
             {!showCart ? (
                 <div className="product-grid">
-
-
+                    {plantsArray.map((category, index) => (
+                        <div className="product-list" key={index}>
+                            <h1>{category.name}</h1>
+                            {category.plants.map((plant, plantIndex) => (
+                                <div className="product-card" key={plantIndex}>
+                                    <img className="product-image" src={plant.image} alt={plant.name}/>
+                                    <div className="product-title">{plant.name}</div>
+                                    <div className='product-description'>{plant.description}</div>
+                                    <div className="product-cost">{plant.cost}</div>
+                                    <button className="product-button" onClick={() => handleAddToCart(plant)}
+                                    >Add to Cart</button>
+                                </div>
+                            ))}
+                        </div>
+                    ))}
                 </div>
             ) : (
                 <CartItem onContinueShopping={handleContinueShopping} />
